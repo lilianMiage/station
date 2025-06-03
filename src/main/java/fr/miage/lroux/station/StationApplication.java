2c61c84c68a2f6e972e5e54c54533686d66f5cb2
Faress-T <tahiri.faress@outlook.com>
2025-06-03 15:26:32 +0200

    Add Eureka Client

--- src/main/java/fr/miage/lroux/station/StationApplication.java
+++ src/main/java/fr/miage/lroux/station/StationApplication.java
@@ -2,8 +2,10 @@ package fr.miage.lroux.station;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StationApplication {
 
 	public static void main(String[] args) {
